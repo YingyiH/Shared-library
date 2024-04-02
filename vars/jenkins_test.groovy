9e19e2b62ce6dc4340dd346afc6e83af7b36cc7b
YingyiH <hedoris8@gmail.com>
2024-04-01 23:07:31 -0700

    Fix bugs

--- vars/jenkins_test.groovy
+++ vars/jenkins_test.groovy
@@ -89,7 +89,7 @@ def call(dockerRepoName, path, imageName) {
                             // rebuilds the Docker containers specified in the 'docker-compose.yml' file located in 
                             // the '/deployment' directory.
                             sh """
-                                ssh -t -t doridori@34.106.187.98 -o StrictHostKeyChecking=no "cd ../deployment && docker compose stop && docker compose rm -f && docker compose pull && docker compose up -d"
+                                ssh -t -t doridori@34.106.187.98 -o StrictHostKeyChecking=no "cd ./deployment && docker compose stop && docker compose rm -f && docker compose pull && docker compose up -d"
                             """
                             // The -o option disables the prompt that asks for confirmation when connecting to a host 
                             // for the first time. This is useful for automation scripts but can be insecure because it 
