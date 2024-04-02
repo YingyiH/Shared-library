660f40f14bdee3bf15a46feec1cc8b9d7925edc5
YingyiH <hedoris8@gmail.com>
2024-04-01 23:33:15 -0700

    Fix bugs

--- backup.groovy
+++ backup.groovy
@@ -1,6 +1,6 @@
 // Named it "call" to call it by default
 
-def call(path, imageName) {
+def call(dockerRepoName, path, imageName) {
     pipeline {
         //Starts defining a Jenkins pipeline and sets it to run on any available agent
         agent any 
@@ -12,17 +12,19 @@ def call(path, imageName) {
         parameters {
             booleanParam(defaultValue: false, description: 'Deploy the App', name: 'DEPLOY')
         }
+        
         stages {
 
             stage('Environment Set up'){
                 steps {
                     script {
+                        sh "rm -rf venv"
                         // Python virtual environment set up
-                        sh 'python3 -m venv venv'
-                        sh '. venv/bin/activate'
+                        sh "python3 -m venv venv"
+                        sh ". venv/bin/activate"
                         // Python package upgrade
-                        sh 'pip install --upgrade pip'
-                        sh 'pip install --upgrade flask'
+                        sh "pip install --upgrade pip"
+                        sh "pip install --upgrade flask"
                     }
                 }
             }
@@ -31,11 +33,11 @@ def call(path, imageName) {
                 steps {
                     script {
                         // Get in Python virtual environment
-                        sh '. venv/bin/activate'
+                        sh ". venv/bin/activate"
                         // Python lint installation
-                        sh 'pip install pylint'
+                        sh "pip install pylint"
                         // Set up pylint minimum score
-                        sh 'pylint --fail-under=5'
+                        sh "pylint --fail-under=5 --disable import-error ./${path}/*.py"
                     }
                 }
             }
@@ -44,33 +46,32 @@ def call(path, imageName) {
                 steps {
                     script{
                         // Get in Python virtual environment
-                        sh '. venv/bin/activate'
+                        sh ". venv/bin/activate"
                         // Install Bandit
-                        sh 'pip install bandit'
+                        sh "pip install bandit"
                         // Runs Bandit to perform security analysis on the code.
-                        sh 'bandit -r ./${path}'
+                        sh "bandit -r ./${path}"
                     }
                 }
             }
             
             stage('Package') {
-                when {
-                    
-                    expression { env.GIT_BRANCH == 'origin/main' }
-                }
+                // when {
+                //     expression {env.GIT_BRANCH == 'origin/main'}
+                // }
                 steps {
                     // Inject credentials securely into the pipeline
-                    withCredentials([string(credentialsId: 'Dockerhub', variable: 'TOKEN')]) {
+                    withCredentials([string(credentialsId: 'DockerHub', variable: 'TOKEN')]) {
                         script {
-                            sh 'cd ${path}'
-                            sh "docker login -u 'yingyi123' -p '$TOKEN' docker.io"
-                            // Build and push image for service
-                            sh '''
-                                docker build -t ${path}:latest --tag yingyi123/${path}:${imageName} .
-                                docker push yingyi/${path}:${imageName}
-                            '''
+                            // Locate path -> Docker login -> Build and push image for service
+                            sh """
+                                cd ./${path}/
+                                docker login -u 'yingyi123' -p '$TOKEN' docker.io
+                                docker build -t ${dockerRepoName}:latest --tag yingyi123/${dockerRepoName}:${imageName} .
+                                docker push yingyi123/${dockerRepoName}:${imageName}
+                            """
                         }
-                    }    
+                    }  
                 }
             }
             
@@ -87,13 +88,12 @@ def call(path, imageName) {
                             // Executes a series of Docker commands on a remote server via SSH. It pulls, and then 
                             // rebuilds the Docker containers specified in the 'docker-compose.yml' file located in 
                             // the '/deployment' directory.
-                            sh '''
-                                sh 'ssh -t -t yhe@34.106.187.98 -o StrictHostKeyChecking=no "cd /deployment"'
-                                sh 'ssh -t -t yhe@34.106.187.98 -o StrictHostKeyChecking=no "docker compose stop"'
-                                sh 'ssh -t -t yhe@34.106.187.98 -o StrictHostKeyChecking=no "docker compose rm -f"'
-                                sh 'ssh -t -t yhe@34.106.187.98 -o StrictHostKeyChecking=no "docker compose pull"'
-                                sh 'ssh -t -t yhe@34.106.187.98 -o StrictHostKeyChecking=no "docker compose up --build -d"'
-                            '''
+                            sh """
+                                ssh -t -t doridori@34.106.187.98 -o StrictHostKeyChecking=no "cd ./deployment && docker compose stop && docker compose rm -f && docker compose pull && docker compose up -d"
+                            """
+                            // The -o option disables the prompt that asks for confirmation when connecting to a host 
+                            // for the first time. This is useful for automation scripts but can be insecure because it 
+                            // makes it vulnerable to man-in-the-middle attacks
                         }
                     }
                 }
@@ -106,7 +106,7 @@ def call(path, imageName) {
                     }
                     script {
                         // Clean up Python virtual environment
-                        sh 'rm -rf venv'
+                        sh "rm -rf venv"
                     }
                 }
             }
