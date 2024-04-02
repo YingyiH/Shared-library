fb9962103df881902995ea37fe09314f47d2eb25
YingyiH <hedoris8@gmail.com>
2024-04-01 17:22:58 -0700

    added new stuff:

--- vars/jenkins_test.groovy
+++ vars/jenkins_test.groovy
@@ -1,43 +1,48 @@
-def call(path, imageName) {
+// Directory is path to service, also docker repo name
+def call(dir, imageName) {
     pipeline {
-        agent any 
-        
-        // parameters {
-        //     booleanParam(defaultValue: false, description: 'Deploy the App', name: 'DEPLOY')
+        agent any
+        // environment {
+        //     PATH = "/var/lib/jenkins/.local/bin:$PATH"
         // }
-        
+        // parameters {
+        //     booleanParam(defaultValue: false, description: 'Deploy the App', name:'DEPLOY')
+        // }
         stages {
-            stage('Environment Set up') {
+            stage('Setup') {
                 steps {
                     script {
-                        sh '''
+                        sh """
                             python3 -m venv venv
                             . venv/bin/activate
+                            pip install pylint
                             pip install --upgrade pip
                             pip install --upgrade flask
-                            pip install pylint
-                        '''
+                            pip install bandit
+                        """
                     }
                 }
             }
-
             stage('Lint') {
                 steps {
                     script {
-                        sh '. venv/bin/activate'
-                        sh 'pylint --fail-under=5'
+                        sh """
+                            . venv/bin/activate
+                            pylint --fail-under=5 --disable import-error ./${dir}/*.py
+                        """
                     }
                 }
             }
-            
-            stage('Security Scan') {
+            stage('Security') {
                 steps {
                     script {
-                        sh ". venv/bin/activate && bandit -r ./${path}"
+                        sh """
+                            . venv/bin/activate
+                            bandit -r ./${dir}
+                        """
                     }
                 }
             }
-            
             // stage('Package') {
             //     when {
             //         expression { env.GIT_BRANCH == 'origin/main' }
@@ -45,35 +50,31 @@ def call(path, imageName) {
             //     steps {
             //         withCredentials([string(credentialsId: 'Dockerhub', variable: 'TOKEN')]) {
             //             script {
-            //                 sh "cd ${path} && docker login -u 'yingyi123' -p '$TOKEN' docker.io"
-            //                 sh "docker build -t ${path}:latest --tag yingyi123/${path}:${imageName} ."
-            //                 sh "docker push yingyi123/${path}:${imageName}"
+            //                 sh """
+            //                 cd ${dir}
+            //                 docker login -u 'tristan007' -p '$TOKEN' docker.io
+            //                 docker build -t ${dir}:latest --tag tristan007/${dir}:${imageName} .
+            //                 docker push tristan007/${dir}:${imageName}
+            //             """
             //             }
-            //         }    
-            //     }
-            // }
-            
-            // stage('Deploy') {
-            //     // when {
-            //     //     expression { params.DEPLOY }
-            //     // }
-            //     steps {
-            //         sshagent(credentials: ['ssh-key']) {
-            //             sh '''
-            //                 ssh -t -t doridori@34.106.187.98 -o StrictHostKeyChecking=no "cd /deployment &&
-            //                 docker compose stop &&
-            //                 docker compose rm -f &&
-            //                 docker compose pull &&
-            //                 docker compose up --build -d"
-            //             '''
             //         }
             //     }
             // }
-
-            // stage('Clean up') {
+            // stage('Deploy') {
+            //     when {
+            //         expression { params.DEPLOY }
+            //     }
             //     steps {
-            //         script {
-            //             sh 'rm -rf venv'
+            //         sshagent(credentials : ['ssh-key']) {
+            //             // https://stackoverflow.com/questions/18522647/run-ssh-and-immediately-execute-command - Run commands using quotes
+            //             sh 'ssh -t -t tristandavis888@34.118.240.191 -o StrictHostKeyChecking=no "cd ./kafka/deployment && docker compose stop && docker compose rm -f && docker compose pull && docker compose up --build -d"'
+            //         }
+            //     }
+            //     post {
+            //         always {
+            //             script {
+            //                 sh 'rm -rf venv'
+            //             }
             //         }
             //     }
             // }
