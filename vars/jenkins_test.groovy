da192ec50aaf1f8e423ba7a528b159445e27c9ce
YingyiH <hedoris8@gmail.com>
2024-04-01 17:51:02 -0700

    asd

--- vars/jenkins_test.groovy
+++ vars/jenkins_test.groovy
@@ -5,7 +5,7 @@ def call(path, imageName) {
         //Starts defining a Jenkins pipeline and sets it to run on any available agent
         agent any 
         environment {
-            PATH = "/var/lib/jenkins/.local/bin:$PATH"
+            PATH = "/var/lib/jenkins/workspace/API-Processing/venv/bin:$PATH"
         }
         // Add a boolean parameter to enable/disable the Delivery stage of the pipeline. 
         // This should go between the agent and stages keywords.
