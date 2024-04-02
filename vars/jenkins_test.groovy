14b7c5649e54625404e85994542109566528f10a
YingyiH <hedoris8@gmail.com>
2024-04-02 00:48:03 -0700

    Fix bugs

--- vars/jenkins_test.groovy
+++ vars/jenkins_test.groovy
@@ -88,9 +88,6 @@ def call(dockerRepoName, path, imageName) {
                             // Executes a series of Docker commands on a remote server via SSH. It pulls, and then 
                             // rebuilds the Docker containers specified in the 'docker-compose.yml' file located in 
                             // the 'deployment' directory.
-                            // sh """
-                            // ssh -t -t doridori@34.106.187.98 -o StrictHostKeyChecking=no "cd /var/lib/jenkins/workspace/API-\'${path}\'/deployment && pwd"
-                            // """
                             sh """
                             ssh -t -t doridori@34.106.187.98 -o StrictHostKeyChecking=no "cd /var/lib/jenkins/workspace/API-\'${path}\'/deployment && docker compose stop && docker compose rm -f && docker compose pull && docker compose up --build -d"
                             """
