8ac831d00ea591fadb72a433d60b8490b34942c4
YingyiH <hedoris8@gmail.com>
2024-04-01 23:14:50 -0700

    Fix bugs

--- vars/jenkins_test.groovy
+++ vars/jenkins_test.groovy
@@ -82,6 +82,9 @@ def call(dockerRepoName, path, imageName) {
                 }
                 steps {
                     script {
+                        sh "pwd && $CURRENT_PATH=$(pwd)"
+                    }
+                    script {
                         // Starts an SSH agent, allowing SSH commands to be executed securely within the pipeline 
                         // using the specified SSH key credentials.
                         sshagent(credentials : ['ssh-key']) {
@@ -89,7 +92,6 @@ def call(dockerRepoName, path, imageName) {
                             // rebuilds the Docker containers specified in the 'docker-compose.yml' file located in 
                             // the '/deployment' directory.
                             sh """
-                                pwd
                                 ssh -t -t doridori@34.106.187.98 -o StrictHostKeyChecking=no "cd ${CURRENT_PATH}/deployment && docker compose stop && docker compose rm -f && docker compose pull && docker compose up -d"
                             """
                             // The -o option disables the prompt that asks for confirmation when connecting to a host 
