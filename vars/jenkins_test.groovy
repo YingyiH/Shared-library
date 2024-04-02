28e919ae9efcb0f3ad15caf67703d84529e29e52
YingyiH <hedoris8@gmail.com>
2024-04-01 20:42:09 -0700

    Test

--- vars/jenkins_test.groovy
+++ vars/jenkins_test.groovy
@@ -18,9 +18,10 @@ def call(path, imageName) {
             stage('Environment Set up'){
                 steps {
                     script {
+                        sh 'pwd'
                         // Python virtual environment set up
-                        // sh 'python3 -m venv venv'
-                        sh '. venv/bin/activate'
+                        sh 'python3 -m venv venv'
+                        sh 'source venv/bin/activate'
                         // Python package upgrade
                         sh 'pip install --upgrade pip'
                         sh 'pip install --upgrade flask'
