48cb73804745c6bbcc6b3dc0b6474a039862f18a
YingyiH <hedoris8@gmail.com>
2024-04-02 09:05:02 -0700

    Fix bugs

--- vars/jenkins_test.groovy
+++ vars/jenkins_test.groovy
@@ -88,8 +88,11 @@ def call(dockerRepoName, path, imageName) {
                             // Executes a series of Docker commands on a remote server via SSH. It pulls, and then 
                             // rebuilds the Docker containers specified in the 'docker-compose.yml' file located in 
                             // the 'deployment' directory.
+                            // sh """
+                            // ssh -t -t doridori@35.235.112.242 -o StrictHostKeyChecking=no "cd ./BESTIE-COMMERCE/deployment && docker compose stop && docker compose rm -f && docker compose pull && docker compose up --build -d"
+                            // """
                             sh """
-                            ssh -t -t doridori@35.235.112.242 -o StrictHostKeyChecking=no "cd ./BESTIE-COMMERCE/deployment && docker compose stop && docker compose rm -f && docker compose pull && docker compose up --build -d"
+                            ssh -t -t doridori@35.235.112.242 -o StrictHostKeyChecking=no "pwd"
                             """
                             // The -o option disables the prompt that asks for confirmation when connecting to a host 
                             // for the first time. This is useful for automation scripts but can be insecure because it 
